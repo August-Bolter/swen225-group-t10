4e25e90571771ca441d2330aeedd85a2265e2f1e
danp <danielpullon@gmail.com>
2019-09-16 15:20:19 +1200

    added a 32x32 board array

--- src/Maze/Board.java
+++ src/Maze/Board.java
@@ -1,4 +1,9 @@
 package Maze;
 
 public class Board {
+    private Tile[][] board = new Tile[32][32];
+
+    public Tile[][] getBoard() {
+        return board;
+    }
 }
--- src/Render/Display.java
+++ src/Render/Display.java
@@ -3,6 +3,8 @@ package Render;
 import javax.swing.*;
 
 public class Display extends JPanel {
+
+
     @Override
     public void repaint() {
         super.repaint();
