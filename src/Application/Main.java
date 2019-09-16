2dda591eeabdeb738deee68c141c40c7a2169053
danp <danielpullon@gmail.com>
2019-09-16 15:11:16 +1200

    Added some panels and the main frame

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,15 +2,18 @@ package Application;
 
 import Maze.Pokeball;
 import Maze.Tile;
+import Render.MainFrame;
 
 import java.util.*;
 
 public class Main {
+    private MainFrame frame;
     private Timer timeRemaining = new Timer();
     private List<Pokeball> allPokeballs = new ArrayList<Pokeball>();
 
 
     private void setup() {
+        frame = new MainFrame();
         timer(5);
     }
 
@@ -23,6 +26,9 @@ public class Main {
         }, seconds*1000);
     }
 
+    public MainFrame getFrame() {
+        return frame;
+    }
 
     public static void main(String[] args) {
         System.out.println("espeon is the best Eevee evo");
