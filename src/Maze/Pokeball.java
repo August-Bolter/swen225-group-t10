f900fd7df8e5f8572de80d87973dc37ef56c8074
Justina <justina.y.koh@gmail.com>
2019-09-16 15:14:12 +1200

    Added in javadocs

--- src/Maze/Pokeball.java
+++ src/Maze/Pokeball.java
@@ -1,5 +1,8 @@
 package Maze;
 
+/**
+ * Class for Pokeball - this is a type of item and it is can be put in the player's inventory
+ */
 public class Pokeball extends Tile{ //extends item? extends tile?
 
     public Pokeball(int row, int col) {
