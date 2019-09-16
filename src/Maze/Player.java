7907ffc4992212950e4b425752fbee07c82ecd39
Justina <justina.y.koh@gmail.com>
2019-09-16 15:07:34 +1200

    Added Player class and Item abstract class

---
+++ src/Maze/Player.java
@@ -0,0 +1,16 @@
+package Maze;
+
+/**
+ * Player class: This is the main player which the user moves to play the game
+ *
+ */
+public class Player {
+    Tile currentPos;
+    //list of items
+
+    Player(Tile startingPos){
+        this.currentPos = startingPos;
+    }
+
+
+}
