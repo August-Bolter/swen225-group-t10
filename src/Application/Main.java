c46f3e85918fc0178f74937600e0203b118ddf9e
Ahad Rahman <rahmanahad@myvuw.ac.nz>
2019-09-21 11:43:01 +1200

    Added resources folder with all our images to use

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,22 +2,28 @@ package Application;
 
 import Maze.*;
 import Persistence.LoadJSON;
+import Render.MainFrame;
 
 import java.util.*;
 
-public class Main {
+public class Main extends java.util.TimerTask {
     private Timer timeRemaining = new Timer(); //Level timer
+    private Timer gameloop;
+    private boolean gameover = false;
     private List<Chip> allChips = new ArrayList<Chip>();
     private int originalNumberOfPokeballs;
     private Player player;
     private LevelBoard levelBoard;
-
+    private MainFrame frame;
 
 
     private void setup() {
-        timer(5);
+        gameloop = new Timer();
+        gameloop.schedule();
+        timer(100);
         createPokeballs(); //Probably don't need this
         levelBoard = LoadJSON.loadLevelFromJSON(1);
+        frame = new MainFrame(this);
     }
 
     /**
@@ -37,16 +43,25 @@ public class Main {
         allChips.add(new Chip(16, 19));
     }
 
-    private boolean doMove(String direction){
+    private boolean doMove(LevelBoard.Direction direction){
         Tile currentPos = player.getCurrentPos();
         Tile desiredTile = levelBoard.getTileAtPosition(currentPos, direction);
-        if (desiredTile.isWalkable()){
+        if (desiredTile != null && desiredTile.isWalkable()) {
             player.setCurrentPos(desiredTile);
+            desiredTile.interact();
+            for (Item item : desiredTile.getItems()){
+                item.interact();
+            }
             return true;
         }
         return false;
     }
 
+    public void run(){
+        while (!gameover){
+            //Render.updateGUI, and other stuff we need to update
+        }
+    }
 
     /**
      * A timer method. Will print game over after a certain amount of time.
@@ -58,10 +73,18 @@ public class Main {
             @Override
             public void run() {
                 System.out.println("-------game over---------");
+                gameover = true;
             }
         }, seconds*1000);
     }
 
+    public MainFrame getFrame() {
+        return frame;
+    }
+
+    public LevelBoard getLevelBoard() {
+        return levelBoard;
+    }
 
     public static void main(String[] args) {
         System.out.println("espeon is the best Eevee evo");
