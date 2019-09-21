bc8a9ef57522b5ce48b70f3417d0ca7b11ee2702
danp <danielpullon@gmail.com>
2019-09-21 15:07:59 +1200

    Board isn't displaying right! Map is drawing the entire map, hard coded 9 x 9 box

--- src/Render/BoardPanel.java
+++ src/Render/BoardPanel.java
@@ -8,9 +8,10 @@ import java.awt.*;
 public class BoardPanel extends JPanel {
     public Tile[][] board;
     public TilePanel[][] boardLabels;
+    public static final int DISPLAY_SIZE = 9;
 
     public BoardPanel(Tile[][] board) {
-        setLayout(new GridLayout(board.length, board[0].length));
+        setLayout(new GridLayout(DISPLAY_SIZE, DISPLAY_SIZE));
         this.board = board;
         boardLabels = new TilePanel[board.length][board[0].length];
 
@@ -45,8 +46,8 @@ public class BoardPanel extends JPanel {
      * Each image will be stored in a map with the class name as the key
      */
     public void redraw() {
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++) {
+        for (int row = 0; row < DISPLAY_SIZE; row++) {
+            for (int col = 0; col < DISPLAY_SIZE; col++) {
                 boardLabels[row][col] = new TilePanel(board[row][col]); // Makes the label, gives it the image for the tile
 //                boardLabels[row][col].setBackground(Color.BLUE);
 //                Graphics g = boardLabels[row][col].getGraphics();
