2b3f81905b92ab39513826db9c52b64938be9bfd
danp <danielpullon@gmail.com>
2019-09-23 14:39:46 +1200

    Added fields to InfoPanel

--- src/Render/InfoPanel.java
+++ src/Render/InfoPanel.java
@@ -1,8 +1,20 @@
 package Render;
 
+import Application.Main;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class InfoPanel extends JPanel {
+    private Main game;
+
+    private JLabel level;
+    private JLabel time;
+    private JLabel chips;
+
+    private JPanel inventoryPanel;
+    private TilePanel[] inventory;
+
 
     /**
      * Info panel needs:
@@ -12,12 +24,16 @@ public class InfoPanel extends JPanel {
      * Inventory (an array with 8 pos) so it'll be 8 labels that can have
      * an image drawn over them
      */
-    public InfoPanel() {
+    public InfoPanel(Main game) {
+        this.game = game;
 
+        setLayout(new GridLayout(4,1));
+
+        display();
     }
 
-    @Override
-    public void repaint() {
-        super.repaint();
+    public void display() {
+        setVisible(true);
     }
+
 }
