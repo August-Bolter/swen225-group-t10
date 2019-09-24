b4f41943030a2a2a0990e9848525d27195998c9a
danp <danielpullon@gmail.com>
2019-09-25 10:55:03 +1200

    Checking for null in TilePanel

--- src/Render/TilePanel.java
+++ src/Render/TilePanel.java
@@ -1,5 +1,6 @@
 package Render;
 
+import Maze.Item;
 import Maze.Tile;
 
 import javax.swing.*;
@@ -32,7 +33,13 @@ public class TilePanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         if (tile.hasItem()) {
-            g.drawImage(tile.getItems().get(0).getImage(), 0, 0, this); // FIXME currently only displaying the first item in the list
+            for (Item i : tile.getItems()) {
+//            if (tile.getItems().get(0) == null) throw new RuntimeException("WRONG ITEM!!");
+                if (i != null) {
+                    g.drawImage(i.getImage(), 0, 0, this); // FIXME currently only displaying the first item in the list
+                    break;
+                }
+            }
         }
     }
 }
