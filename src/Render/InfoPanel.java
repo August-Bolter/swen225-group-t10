e783ef459a782420c9088f2ad49667d5d03f772b
danp <danielpullon@gmail.com>
2019-09-24 15:51:38 +1200

    Redoing InfoPanel

--- src/Render/InfoPanel.java
+++ src/Render/InfoPanel.java
@@ -1,8 +1,18 @@
 package Render;
 
+import Application.Main;
+import Maze.Item;
+
 import javax.swing.*;
 
 public class InfoPanel extends JPanel {
+    Main game;
+
+    JLabel level;
+    JLabel timeRemaining;
+    JLabel chipsRemaining;
+
+    Item[] inventory;
 
     /**
      * Info panel needs:
@@ -12,12 +22,11 @@ public class InfoPanel extends JPanel {
      * Inventory (an array with 8 pos) so it'll be 8 labels that can have
      * an image drawn over them
      */
-    public InfoPanel() {
+    public InfoPanel(Main game) {
+        this.game = game;
+        inventory = game.getPlayer().getInventory();
 
-    }
 
-    @Override
-    public void repaint() {
-        super.repaint();
+
     }
 }
