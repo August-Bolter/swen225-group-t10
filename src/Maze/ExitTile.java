43945f22ee316de285d6fe2ad634ec1bb84d18ec
Justina <justina.y.koh@gmail.com>
2019-10-09 19:39:51 +1300

    Fixed javadocs, rearranged code and added comments for firetile

--- src/Maze/ExitTile.java
+++ src/Maze/ExitTile.java
@@ -3,6 +3,7 @@ package Maze;
 /** Represents an exit tile in the game. When a player steps on an exit tile the level ends (as they have completed the level)
  *  and the new level is loaded in. */
 public class ExitTile extends Tile {
+
     /** Creates an exit tile
      * @param row The row (in regards to the board) of the exit tile
      * @param col The column (in regards to the board) of the exit tile
--- src/Maze/FireTile.java
+++ src/Maze/FireTile.java
@@ -1,7 +1,16 @@
 package Maze;
 
+/**
+ * FireTile this is a type of tile which the players can't interact with (walk on) unless they have obtained the boots
+ * If they walk on the firetile without boots the game will restart
+ */
 public class FireTile extends Tile {
 
+    /**
+     * Constructor for the fire tile
+     * @param row of where the firetile is located
+     * @param col of where the firetile is located
+     */
     public FireTile(int row, int col) {
         super(row, col);
     }
@@ -11,6 +20,11 @@ public class FireTile extends Tile {
         return main.getPlayer().isInInventory(new Boots(0,0));
     }
 
+    /**
+     * Method that allows the player to interact with the firetile
+     * If the player enters/walk ontop of the firetiel without the boots, then the game will instantly restart
+     * If the player has the boots, then they can freely interact (walk) on the firetile
+     */
     @Override
     public void interact() {
 
