a567e3cd51a67331af8f888c8a3a96be443a20f0
Justina <justina.y.koh@gmail.com>
2019-10-09 19:32:39 +1300

    Fixed javadocs, rearranged code and added comments for Enemy

--- src/Maze/Enemy.java
+++ src/Maze/Enemy.java
@@ -6,11 +6,21 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Enemy class: this is a parent class for all enemies: blueEnemy, greenEnemy and redEnemy
+ */
 public class Enemy extends Item {
+    //start of fields
     protected Tile currentPos;
-    protected Maze.LevelBoard.Direction direction;
-    private int row, col;
+    protected Maze.LevelBoard.Direction direction;  //direction enemy is travelling in
+    private int row, col;       //location of the enemy
 
+    /**
+     * Constructor for an enemy
+     * @param row The row (in regards to the board) of the chip
+     * @param col The column (in regards to the board) of the chip
+     * @param direction enemy is travelling in
+     */
     public Enemy(int row, int col, String direction){
         super(row, col);
         this.row = row;
@@ -19,6 +29,10 @@ public class Enemy extends Item {
         this.setPriority(5);
     }
 
+    /**
+     * Method used to 'interact' (when the player steps on a square the enemy is on)
+     * Note: the level will restart if this occurs
+     */
     @Override
     public void interact() {
 //        //Kill player, game over
@@ -30,6 +44,10 @@ public class Enemy extends Item {
 //        }
     }
 
+    /**
+     * Makes the enemy move from one tile to another tile
+     * @param toMoveTo - tile it is moving to
+     */
     public void doMove(Tile toMoveTo){
         currentPos.removeItem(this);
         toMoveTo.addItem(this);
@@ -60,31 +78,52 @@ public class Enemy extends Item {
         }
     }
 
+
+    //start of getters and setters
+
+    /**
+     * Getter method: fetches the current position of the enemy
+     * @return currentPos - the tile the enemy is currently on
+     */
     public Tile getCurrentPos() {
         return currentPos;
     }
 
+    /**
+     * Getter method: fetches the direction of the enemy
+     * @return direction - direction enemy is going to move in
+     */
     public LevelBoard.Direction getDirection() {
         return direction;
     }
 
+    /**
+     * Getter method: fetches the row of the enemy
+     * @return row - row enemy is currently on
+     */
     @Override
     public int getRow() {
         return row;
     }
 
+    /**
+     * Getter method: fetches the col of the enemy
+     * @return col - col enemy is currently on
+     */
     @Override
     public int getCol() {
         return col;
     }
 
+    /**
+     * Setter method: sets the row of the enemy
+     * In one sense: updates the current position to the position the enemy has moved onto
+     */
     public void setCurrentPos(Tile currentPos) {
         this.currentPos = currentPos;
     }
 
-    /**
-     * Sets the current position of the Enemy by getting a tile from the board
-     */
+
     public void setCurrentPos() {
         this.currentPos = main.getLevelBoard().getBoard()[row][col];
     }
