abf039f258e832a44713714381e0901f59d324a1
Justina <justina.y.koh@gmail.com>
2019-10-09 19:36:24 +1300

    Fixed javadocs, rearranged code and added comments for fireblast

--- src/Maze/Fireblast.java
+++ src/Maze/Fireblast.java
@@ -2,12 +2,18 @@ package Maze;
 
 import Application.Main;
 
+/**
+ * Fireblast class - used by the redenemy
+ */
 public class Fireblast extends RedEnemy {
     public Fireblast(int row, int col, String direction, Main main) {
         super(row, col, direction);
         this.main = main;
     }
 
+    /**
+     * Moves the blast in one specific direction
+     */
     public void moveBlast(){
         Tile newTile = main.getLevelBoard().getTileAtPosition(currentPos, direction);
         if (newTile.isWalkable()) {
@@ -20,6 +26,10 @@ public class Fireblast extends RedEnemy {
         }
     }
 
+    /**
+     * Method for interacting with the fireblast - this occurs when the player walks into the same tile as the fireblast
+     * When this occurs, the game starts again
+     */
     public void interact() {
         main.getFrame().displayInfo("Watch out for Fire Blasts!");
         main.restartLevel();
