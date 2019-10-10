09b346de3a5113b9e45f5bf3e07ed80fb7493d98
lockwohugh <Huoogle1>
2019-10-10 14:12:43 +1300

    Minor fix

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -108,7 +108,7 @@ public class Main {
      */
     public void setup() {
         //setting up the correct level
-        //initialiseMaps();
+        initialiseMaps();
         levelBoard = LoadJSON.loadLevelFromJSON(level);
         levelBoard.setMain(this);
 
