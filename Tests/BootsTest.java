f85bd1d774c16d5050a2a723fc159c505f42033d
Justina <justina.y.koh@gmail.com>
2019-10-10 18:41:23 +1300

    Added some tests

---
+++ Tests/BootsTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootsTest {
+
+    @Test
+    void interact() {
+    }
+
+    @Test
+    void getImage() {
+    }
+}
