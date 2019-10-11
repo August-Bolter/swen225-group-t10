152bad791f7a726855fe186a1d67ca8c0d617f29
Justina <justina.y.koh@gmail.com>
2019-10-11 13:04:12 +1300

    Added some more test cases

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -1,34 +1,44 @@
 import Application.Main;
+import Maze.LevelBoard;
+import Persistence.LoadJSON;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
     /**
-     * Tests whether a board is made
-     */
-    @Test
-    public void createBoardTest(){
-   //     Main game = new Main(1);
-//        game.setup(2);
-
-    }
-
-    /**
-     * Tests a valid move
+     * Tests an invalid move
      * @return
      */
     @Test
-    public void doValidMove() {
-        //get class get methods
-        Method[]  methodList = getClass().getDeclaredMethods();
-        for(Method m :methodList){
-            if(m.getName() == "doMove"){
-                Class c = m.getDeclaringClass();
+    public void doInvalidMove() throws InterruptedException {
+        Thread t = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try{
+                    Main m = new Main("tester", 1);
+                    m.setup(1);
+                    m.doMove(LevelBoard.Direction.DOWN);    //shouldn't be able to move down
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
             }
-        }
+        });
+        t.start();
+        Thread.sleep(1000);
+        assertTrue(true);   //therefore if we reach here, then it must be working
+
+        //get class get methods
+//        Method[]  methodList = getClass().getDeclaredMethods();
+//        for(Method m :methodList){
+//            if(m.getName() == "doMove"){
+//                Class c = m.getDeclaringClass();
+//            }
+//        }
+//        assertTrue(true);
     }
 
 
---
+++ Tests/TextToJSONTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class for testing whether the JSON works
+ */
+class TextToJSONTest {
+
+    @Test
+    void main() {
+        Assert.assertNotSame("Level-11.json", "Level-12.json" );
+    }
+}
